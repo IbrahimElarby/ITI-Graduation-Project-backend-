2272b80a5b4938de35e454154b1da7e00fb89364
MoazTamer <moaztamer390@gmail.com>
2025-04-08 06:48:32 +0200

    Cleaned up .vs merge conflicts after stash pop

---
+++ ITIGraduationProject BackEnd/GraduationProject.BL/DTO/RecipeManger/Write/RecipeCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITIGraduationProject.BL.DTO.RecipeManger.Input
+{
+    public class RecipeCreateDto
+    {
+        public string Title { get; set; }
+        public string Instructions { get; set; }
+        public int PrepTime { get; set; }
+        public string Description { get; set; }
+        public int CookingTime { get; set; }
+        public string CuisineType { get; set; }
+
+        // IDs فقط (بدلاً كائنات كاملة)
+        public int CreatedBy { get; set; }
+        public List<int> CategoryIds { get; set; }
+        public List<RecipeIngredientCreateDto> Ingredients { get; set; }
+    }
+}
---
+++ ITIGraduationProject BackEnd/ITIGraduationProject/Controllers/RecipeController.cs
@@ -0,0 +1,25 @@
+﻿using ITIGraduationProject.BL.DTO.RecipeManger.Output;
+using ITIGraduationProject.BL.Manger;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITIGraduationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeManger _recipeManager;
+        public RecipeController(IRecipeManger recipeManger)
+        {
+            _recipeManager = recipeManger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<RecipeDetailsDTO>>> GetAll()
+        {
+            var recipes = await _recipeManager.GetAll();
+            return Ok(recipes);
+        }
+    }
+}
